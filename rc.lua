d8a9b795ae8a68e13879b6f0666a4abbdf1bb1f5
gilsongindrejr <gindrejunior@hotmail.com>
2022-09-04 10:48:39 -0300

    refactor: moved mytasklist to another file

--- rc.lua
+++ rc.lua
@@ -37,6 +37,7 @@ local systray = require("widgets.systray")
 local mypromptbox = require("widgets.mypromptbox")
 local mylauncher = require("widgets.mylauncher")
 local mytaglist = require("widgets.mytaglist")
+local mytasklist = require("widgets.mytasklist")
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -99,28 +100,6 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibar
 -- Create a wibox for each screen and add it
-local tasklist_buttons = gears.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  c:emit_signal(
-                                                      "request::activate",
-                                                      "tasklist",
-                                                      {raise = true}
-                                                  )
-                                              end
-                                          end),
-                     awful.button({ }, 3, function()
-                                              awful.menu.client_list({ theme = { width = 250 } })
-                                          end),
-                     awful.button({ }, 4, function ()
-                                              awful.client.focus.byidx(1)
-                                          end),
-                     awful.button({ }, 5, function ()
-                                              awful.client.focus.byidx(-1)
-                                          end))
-
 local function set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
@@ -158,11 +137,7 @@ awful.screen.connect_for_each_screen(function(s)
     s.mytaglist = mytaglist(s)
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = tasklist_buttons
-    }
+    s.mytasklist = mytasklist(s)
 
     -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s })
